ee52624c67447d3e9e1fa515b7f5affef62d467e
acm-wq <camisglhdadae@gmail.com>
2024-10-16 14:36:57 +0000

    add blur method commit

--- lib/venus.rb
+++ lib/venus.rb
@@ -136,4 +136,52 @@ module Venus
       [input_file1, input_file2, output_file]
     end
   end
+
+  class BlurVideo
+    def self.blur_video(input_file, output_file, path: nil, blur_intensity: 20)
+      unless File.exist?(input_file)
+        puts "File not found: #{input_file}"
+        exit 1
+      end
+  
+      # Проверка на допустимость blur_intensity
+      unless blur_intensity.is_a?(Numeric) && blur_intensity >= 0
+        puts "Invalid blur intensity. It should be a non-negative number."
+        exit 1
+      end
+  
+      output_file = "#{output_file}.mp4" unless output_file.end_with?('.mp4')
+  
+      output_path = path ? File.join(path, output_file) : File.join(Dir.pwd, output_file)
+  
+      command = "ffmpeg -i #{input_file} -vf 'boxblur=#{blur_intensity}:1' -c:a copy #{output_path}"
+  
+      Open3.popen3(command) do |_stdin, _stdout, stderr, wait_thr|
+        while line = stderr.gets
+          puts line
+        end
+  
+        exit_status = wait_thr.value
+        unless exit_status.success?
+          puts "Error executing command: #{command}"
+          exit 1
+        end
+      end
+    end
+  
+    def self.process_arguments(args_string)
+      args = args_string.split
+  
+      # Проверяем количество аргументов
+      if args.length < 2 || args.length > 4
+        puts 'Usage: ruby main.rb <input_file> <output_file> [path] [blur_intensity]'
+        exit 1
+      end
+  
+      input_file = args[0]
+      output_file = args[1]
+  
+      [input_file, output_file]
+    end
+  end
 end
